152bf55b7973b8fabad8425c121a05e7cba64801
Chuchullo_Flores_John_Ruben <201123187j@utea.edu.pe>
2024-04-17 12:13:54 -0500

    se Establece conexion hacia la base de datos ventas Sql server

---
+++ Ventas_de_boletos_RECUPERACION/SqlConnection .cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+
+namespace Ventas_de_boletos_RECUPERACION
+{
+    public class SqlConnection
+    {
+      
+        {
+            // Cadena de conexión a la base de datos
+            string connectionString = "Data Source=DESKTOP-VIITSAH\RUBE;Initial Catalog=BDVenta_Boletos;User ID=sa;Password=12345678";
+        }
+}
