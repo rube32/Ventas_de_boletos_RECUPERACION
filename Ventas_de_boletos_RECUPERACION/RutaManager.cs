f1af5b03196d1a1d18c5d8852a6d248d9474580e
Chuchullo_Flores_John_Ruben <201123187j@utea.edu.pe>
2024-04-17 10:41:38 -0500

    Se define  Rutas  al proyecto

---
+++ Ventas_de_boletos_RECUPERACION/RutaManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ventas_de_boletos_RECUPERACION
+{
+    public class RutaManager
+    {
+        
+            static void Main(string[] args)
+            {
+            // Definir las rutas
+            var rutas = new Ruta[] {
+            new Ruta("Sacsayhuaman - Puka Pukara - Tambomachay", 100),
+            new Ruta("Tipon - Lucre - Piquillaqta", 120),
+            new Ruta("Ollantaytambo - Machupicchu", 150);
+       
+
+            }
+    }
+
+}
