e5d8b8ba9539b7c99c8e1c1cacdd240e6e9b79d1
Chuchullo_Flores_John_Ruben <201123187j@utea.edu.pe>
2024-04-17 10:29:13 -0500

    Se define la clase Ruta al proyecto

---
+++ Ventas_de_boletos_RECUPERACION/Ruta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ventas_de_boletos_RECUPERACION
+{
+    class Ruta
+    {
+        public string Nombre { get; set; }
+        public decimal PrecioBase { get; set; }
+        public Ruta(string nombre, decimal precioBase)
+        {
+            Nombre = nombre;
+            PrecioBase = precioBase;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
