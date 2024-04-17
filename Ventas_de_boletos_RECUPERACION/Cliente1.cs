9bb95319a53fc910481be3673d18faf6d7b5c194
Chuchullo_Flores_John_Ruben <201123187j@utea.edu.pe>
2024-04-17 10:27:23 -0500

    Se define la clase Cliente al proyecto

---
+++ Ventas_de_boletos_RECUPERACION/Cliente1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ventas_de_boletos_RECUPERACION
+{
+    class Cliente
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+        public bool EsPromocionColegios { get; set; }
+        public Cliente(string nombre, int edad, bool esPromocionColegios)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            EsPromocionColegios = esPromocionColegios;
+        }
+    }
+}
